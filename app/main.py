b67183cac79ec5c451ae2e1d4b98482279b67a3c
Mikhail Pakhomenkov <pahomenkov@siberian.pro>
2024-05-22 20:29:38 +0300

    Delete useless code

--- app/main.py
+++ app/main.py
@@ -8,9 +8,3 @@ setattr(app.state, "db", DB())
 setattr(app.state, "transactions", {})
 
 app.include_router(v1)
-
-
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host='0.0.0.0', port=8082, reload=True)
